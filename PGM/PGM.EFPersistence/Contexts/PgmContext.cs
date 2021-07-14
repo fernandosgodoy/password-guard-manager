6431479857758178a68b5d33b29361d728fadd65
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-07-14 12:13:05 -0300

    created the persistence to EntityFramework Core project and created the DbContext

---
+++ PGM/PGM.EFPersistence/Contexts/PgmContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PGM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGM.EFPersistence.Contexts
+{
+    public class PgmContext
+        : DbContext
+    {
+
+        #region DbSets
+
+        public DbSet<AccountInfo> Accounts { get; set; }
+
+        #endregion
+
+    }
+}
